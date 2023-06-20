374bdb26113b0eb7c77bcaff707f3e57d893ada0
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2023-06-20 21:59:42 +0100

    Changed navbar buttons and dropdown menu.

--- client/src/lib/components/home/NavbarAuthSection.svelte
+++ client/src/lib/components/home/NavbarAuthSection.svelte
@@ -1,26 +1,26 @@
 <div class="flex gap-4 items-center">
 
-    <!-- Add New Button -->
-    <a href="/new" class="btn btn-sm btn-accent">NEW</a>
-
     <!-- Dashboard Button -->
-    <a href="/user" class="btn btn-outline btn-sm">Dashboard</a>
+    <a href="/user" class="btn btn-sm">Dashboard</a>
+
+    <!-- Add New Button -->
+    <a href="/new" class="btn btn-sm btn-accent">Add New</a>
 
     <div class="dropdown dropdown-end">
 
-        <!-- Avatar / Hamburger Menu -->
-        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-            <div class="w-10 rounded-full">
-                <img src="/logo-textless.svg" />
-            </div>
-        </label>
+        <div class="dropdown dropdown-hover">
+            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                <div class="w-10 rounded-full">
+                    <img src="/icons/avatar.svg" />
+                </div>
+            </label>
 
-        <!-- Menu Items -->
-        <ul tabindex="0" class="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-            <li><a href="/user">Profile</a></li>
-            <li><a href="/">Settings</a></li>
-            <li><a href="/">Logout</a></li>
-        </ul>
+            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a href="/user">Profile</a></li>
+                <li><a href="/">Settings</a></li>
+                <li><a href="/">Logout</a></li>
+            </ul>
+        </div>
 
     </div>
 </div>
--- client/src/lib/components/home/NavbarNoAuthSection.svelte
+++ client/src/lib/components/home/NavbarNoAuthSection.svelte
@@ -1,9 +1,9 @@
 <div class="flex gap-4 items-center">
 
-    <!-- Add New Button -->
-    <a href="/login" class="btn btn-sm btn-accent">LOGIN</a>
-
     <!-- Dashboard Button -->
     <a href="/register" class="btn btn-outline btn-sm">SIGN UP</a>
+
+    <!-- Add New Button -->
+    <a href="/login" class="btn btn-sm btn-accent">LOGIN</a>
 
 </div>
