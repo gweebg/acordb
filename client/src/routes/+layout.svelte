637744b2ea536f54e86ddb3f59dc808efbaced38
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2023-06-24 14:20:09 +0100

    Added full loading animations.

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script>
     import '../app.scss';
+
+    import {navigating} from "$app/stores";
+    import {loading} from "$lib/stores/loading.js";
+
+    import Loading from "$lib/components/Loading.svelte";
+
+    $: $loading = !!$navigating
 </script>
 
-<slot></slot>
+<Loading/>
 
-<style>
+<slot/>
 
-</style>
