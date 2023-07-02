3873eba873009d77057ec4063afc4c6f7b40017b
eivarin <rodrigommpereira309@gmail.com>
2023-07-02 23:44:36 +0100

    turned off debug mode

--- server/acordãos/settings.py
+++ server/acordãos/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1', 'api-server','*','172.26.113.96', 'eivarin.xyz']
 
